0c505e8ff49f76f91f96c83c0b26778cec4493eb
miuwang <jocelyn.hello519@gmail.com>
2018-06-08 18:41:57 +0800

    Update oo.php

--- oo.php
+++ oo.php
@@ -1,1 +1,23 @@
-# seo
+<div class="list-group-item list-group-item-action flex-column align-items-start " itemscope=""
+     itemtype="https://schema.org/ImageObject">
+
+    <a href="/176921" title="如果你試圖不斷的觸動金牛座的底線，激怒他，你得到的可能是最恐怖的報復！">
+        <div class="w3-col l4 w3-center tag-item">
+            <img src="/focus_photos/42408.jpg" itemprop="contentUrl" alt="如果你試圖不斷的觸動金牛座的底線，激怒他，你得到的可能是最恐怖的報復！"
+                 class="img-fluid cat-img">
+        </div>
+    </a>
+    <div class="w3-col l8">
+        <span itemprop="datePublished" class="d-none" content="2018-06-08"> 2018-06-08</span>
+        <span itemprop="author" class="d-none">polly</span>
+        <p class="list-tag"><span rel="tag" class="list-tag-span" onclick="window.open('/tag/金牛座','_self')">金牛座</span>
+        </p>
+        <p class="list-tag"><span rel="tag" class="list-tag-span" onclick="window.open('/tag/激怒','_self')">激怒</span></p>
+        <p class="list-tag"><span rel="tag" class="list-tag-span" onclick="window.open('/tag/底線','_self')">底線</span></p>
+        <p class="list-tag"><span rel="tag" class="list-tag-span" onclick="window.open('/tag/恐怖','_self')">恐怖</span></p>
+        <a href="/176921"><h5 itemprop="name" class="list-title">如果你試圖不斷的觸動金牛座的底線，激怒他，你得到的可能是最恐怖的報復！</h5>
+            <p class="list-desc" itemprop="description">
+                金牛座的人通常脾氣很好，因為他們有著極強的壓抑心裏，可以將任何情緒掩蓋在外表之下，並且你要相信，他有這個本事一忍到底。通常他們不發作是為了不讓別人難堪，金牛座很愛自己的面子，自尊心極強，正是因為如此，...</p>
+        </a>
+    </div>
+</div>
